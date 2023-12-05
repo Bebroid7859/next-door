b3eb4cd17217c6f8ca10ae979358b9341caffbe1
koronisk <106849559+koronisk@users.noreply.github.com>
2023-12-06 00:23:52 +0200

    Небольшая загрузка, или как это назвать

--- NextDoor/Game.cs
+++ NextDoor/Game.cs
@@ -4,9 +4,35 @@ namespace NextDoor
     {
         public static Logger Logger = new(true);
 
-        public static void Main()
+        static void Main()
+        {
+            try
+            {
+                Logger.Write(LogType.Info, "Инициализация...");
+                Initialize();
+                Logger.Write(LogType.Info, "Всё в порядке, запуск игры.");
+                Run();
+            }
+            catch (Exception exception)
+            {
+                Logger.Write(LogType.Crash, "Не всё в порядке. Записываю подробнее в файл ./crash-log.txt");
+                File.WriteAllText("./crash-log.txt", exception.ToString());
+            }
+        }
+
+        static void Initialize()
+        {
+            // здесь что-то загружается...
+        }
+
+        static void Run()
         {
             Logger.Write(LogType.Info, "Запустилось!");
+
+            while (true)
+            {
+
+            }
         }
     }
 }
